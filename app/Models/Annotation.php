0358401aea55aff6b26fdd14f882dcc2e0858ce8
Aris Kosmopoulos <arisgl@gmail.com>
2026-04-01 15:06:02 +0300

    work in progress on models

--- app/Models/Annotation.php
+++ app/Models/Annotation.php
@@ -4,8 +4,16 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Database\Factories\AnnotationFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Annotation extends Model {
-    //
+    /** @use HasFactory<AnnotationFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'annotation_assignment_id',
+        'user_id',
+    ];
 }
--- app/Models/AnnotationAssignment.php
+++ app/Models/AnnotationAssignment.php
@@ -4,10 +4,17 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Database\Factories\AnnotationAssignmentFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class AnnotationAssignment extends Model {
-    /** @use HasFactory<\Database\Factories\AnnotationAssignmentFactory> */
+    /** @use HasFactory<AnnotationAssignmentFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'annotation_instance_id',
+        'user_id',
+        'sub_project_id',
+    ];
 }
