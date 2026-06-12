11a770980cc640c47ea1e99da28e14b205761364
Paul Isaris <paulisaris@gmail.com>
2026-06-12 14:51:52 +0300

    added missing translations

--- lang/el/api.php
+++ lang/el/api.php
@@ -3,8 +3,8 @@
 declare(strict_types=1);
 
 return [
-    'rating_saved' => 'Rating saved successfully',
-    'rating_failed' => 'Failed to save rating',
-    'logout_successful' => 'Logout successful',
-    'logout_failed' => 'Failed to logout',
+    'rating_saved' => 'Η αξιολόγηση αποθηκεύτηκε επιτυχώς',
+    'rating_failed' => 'Αποτυχία αποθήκευσης αξιολόγησης',
+    'logout_successful' => 'Η αποσύνδεση ήταν επιτυχής',
+    'logout_failed' => 'Αποτυχία αποσύνδεσης',
 ];
